b78767aee8e859bf31494e43e405634fc3c7c408
duogbachdev <contact.duogbachdev@gmail.com>
2023-10-28 22:07:54 +0700

    App Bar

---
+++ src/components/AppBar/Menus/Templates.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Box, Button, Divider, ListItemIcon, ListItemText, Menu, MenuItem } from '@mui/material'
+import { Check } from '@mui/icons-material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+
+const Templates = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  return (
+    <Box>
+      <Button
+        id="basic-button-templates"
+        aria-controls={open ? 'basic-menu-templates' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        endIcon=<ExpandMoreIcon />
+      >
+        Templates
+      </Button>
+      <Menu
+        id="basic-menu-templates"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button-templates'
+        }}
+      >
+        <MenuItem>
+          <ListItemText inset>Single</ListItemText>
+        </MenuItem>
+        <MenuItem>
+          <ListItemText inset>1.15</ListItemText>
+        </MenuItem>
+        <MenuItem>
+          <ListItemText inset>Double</ListItemText>
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Check />
+          </ListItemIcon>
+          Custom: 1.2
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemText>Add space before paragraph</ListItemText>
+        </MenuItem>
+        <MenuItem>
+          <ListItemText>Add space after paragraph</ListItemText>
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemText>Custom spacing...</ListItemText>
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Templates
