d974f9efffde11fe75f25ebdb2b5fc5ba4db9a83
duogbachdev <contact.duogbachdev@gmail.com>
2023-11-04 15:47:24 +0700

    Axios Call API

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,13 +3,25 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 const Board = () => {
+  const [board, setBoard] = useState(null)
+  useEffect(() => {
+    // lấy chuẩn boardId từ url về dùng react-router-dom
+    const boardId = '6545f6a096ded2660db1e6fd'
+    // Call api
+    fetchBoardDetailsAPI(boardId)
+      .then(board => {
+        setBoard(board)
+      })
+  }, [])
 
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={ mockData?.board }/>
-      <BoardContent board={ mockData?.board }/>
+      <BoardBar board={ board }/>
+      <BoardContent board={ board }/>
     </Container>
   )
 }
